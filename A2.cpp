030b4f0323bf0ddc00729402063bb0a0cad5605a
Brigham <kathleen.brigham@gmail.com>
2014-06-13 21:32:35 -0700

    ...

--- A2.cpp
+++ A2.cpp
@@ -19,7 +19,10 @@ which the process/thread is accessing as it performs it’s work. The limit of n
 pages any process/thread may have is 99. The pages.txt file will contain a single line of 
 text where each number is separated by a space character.
 */
+
+promptUser(){
 //Upon startup, prompt the user for the number of frames in main memory 
+}
 //Read the sequence of pages accessed from the file "pages.txt" : The 
 //sequence of pages the page/thread accesses will be contained here This file shall be located in the same directory as the executable 
 //and shall contain a series of positive integers representing the pages (by it’s page number) which the process/thread is accessing as 
