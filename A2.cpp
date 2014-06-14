28c83d4734a300fb0f1dba7ad68d05e1726aa0a9
Brigham <kathleen.brigham@gmail.com>
2014-06-13 21:21:46 -0700

    formatting

---
+++ A2.cpp
@@ -0,0 +1,47 @@
+//================================================================
+// Name        : A2.cpp
+// Author      : Katie Brigham MASC1420, Shawn Vega MASC1440
+// Description : Assignment 2, CS570 Summer 2014
+//================================================================
+#include "A2.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+/*
+This program shall simulate a process/thread accessing its pages during execution. 
+which you shall create. This file shall be located in the same directory as the executable 
+and shall contain a series of positive integers representing the pages (by it’s page number) 
+which the process/thread is accessing as it performs it’s work. The limit of number of 
+pages any process/thread may have is 99. The pages.txt file will contain a single line of 
+text where each number is separated by a space character.
+*/
+//Upon startup, prompt the user for the number of frames in main memory 
+//Read the sequence of pages accessed from the file "pages.txt" : The 
+//sequence of pages the page/thread accesses will be contained here This file shall be located in the same directory as the executable 
+//and shall contain a series of positive integers representing the pages (by it’s page number) which the process/thread is accessing as 
+//it performs it’s work.The pages.txt file will contain a single line of text where each number is separated by a space character.
+
+
+//Run each of the algorithms (OPT, LRU, and Clock) on the same input string from the step above 
+
+
+//Printout the number of faults each algorithm generates to the terminal. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.txt
@@ -0,0 +1,16 @@
+//================================================================
+// Name        : README
+// Author      : Katie Brigham MASC1420, Shawn Vega MASC1440
+// Description : Assignment 2, CS570 Summer 2014
+//================================================================
+File manifest:
+
+Compile instructions:
+
+Operating instructions (include any/all command line options/arguments, required inputs, etc):
+
+List/description of novel/significant design decisions:
+
+List/description of any extra features/algorithms/functionality:
+
+List/description of all known:
